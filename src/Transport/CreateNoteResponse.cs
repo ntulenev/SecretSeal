36aaf4f4248f68bf4a9e2d1a725cd0b843eb6856
Nikita Tulenev <n1tulenev@gmail.com>
2026-01-17 10:41:07 +0100

    Adds CreateNoteResponse record
    Introduces a record to encapsulate the result of a create note operation, including the ID of the newly created note.

---
+++ src/Transport/CreateNoteResponse.cs
@@ -0,0 +1,7 @@
+namespace Transport;
+
+/// <summary>
+/// Represents the result of a create note operation, including the unique identifier of the newly created note.
+/// </summary>
+/// <param name="Id">The unique identifier assigned to the newly created note.</param>
+public sealed record CreateNoteResponse(Guid Id);
