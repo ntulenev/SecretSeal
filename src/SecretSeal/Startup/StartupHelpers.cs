f91917c3f3e8bff90a459ac87ecc9150810c21eb
Nikita Tulenev <n1tulenev@gmail.com>
2026-01-17 22:33:19 +0100

    Configures application services and runtime
    Sets up dependency injection based on storage configuration.
    
    Configures storage either in memory or using a database, and
    registers the necessary services for each mode.
    
    Ensures the database is created during application startup
    when using database storage.

---
+++ src/SecretSeal/Startup/StartupHelpers.cs
@@ -0,0 +1,98 @@
+using System.Diagnostics;
+
+using Abstractions;
+
+using Cryptography;
+using Cryptography.Configuration;
+
+using Logic;
+
+using Microsoft.EntityFrameworkCore;
+
+using Models;
+
+using SecretSeal.Configuration;
+
+using Storage;
+using Storage.Repositories;
+
+namespace SecretSeal.Startup;
+
+/// <summary>
+/// 
+/// </summary>
+internal static class StartupHelpers
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="args"></param>
+    /// <returns></returns>
+    /// <exception cref="InvalidOperationException"></exception>
+    /// <exception cref="UnreachableException"></exception>
+    public static WebApplication CreateApplication(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        _ = builder.Services.AddSingleton<ICryptoHelper, CryptoHelper>();
+        _ = builder.Services
+            .AddOptions<CryptoOptions>()
+            .Bind(builder.Configuration.GetSection("Crypto"))
+            .ValidateDataAnnotations()
+            .ValidateOnStart();
+
+        var storageOption = builder.Configuration
+                    .GetSection("Storage")
+                    .Get<StorageOptions>()
+                    ?? throw new InvalidOperationException("Storage options are not configured.");
+
+        switch (storageOption.Mode)
+        {
+            case StorageMode.InMemory:
+                _ = builder.Services.AddSingleton<INotesHandler, CryptoNotesHandler>();
+                _ = builder.Services.AddSingleton<INotesHandler, InMemoryNotesHandler>();
+                break;
+
+            case StorageMode.Database:
+                _ = builder.Services.AddScoped<INotesHandler, CryptoNotesHandler>();
+                var cs = builder.Configuration.GetConnectionString("SecretSealDb");
+                if (string.IsNullOrWhiteSpace(cs))
+                {
+                    throw new InvalidOperationException("ConnectionStrings:SecretSealDb is not configured");
+                }
+                _ = builder.Services.AddDbContext<SecretSealDbContext>(o => o.UseSqlServer(cs));
+                _ = builder.Services.AddScoped<IUnitOfWork, EfUnitOfWork>();
+                _ = builder.Services.AddScoped<IRepository<Note, NoteId>, NotesRepository>();
+                _ = builder.Services.AddScoped<INotesHandler, StorageNotesHandler>();
+                break;
+
+            default:
+                throw new UnreachableException($"Unreachable case for storage {storageOption.Mode} mode");
+        }
+
+        _ = builder.Services.Decorate<INotesHandler, CryptoNotesHandler>();
+        return builder.Build();
+
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="app"></param>
+    /// <returns></returns>
+    /// <exception cref="InvalidOperationException"></exception>
+    public static async Task RunAppAsync(WebApplication app)
+    {
+        var storageOption = app.Configuration
+                   .GetSection("Storage")
+                   .Get<StorageOptions>()
+                   ?? throw new InvalidOperationException("Storage options are not configured.");
+        if (storageOption.Mode == StorageMode.Database)
+        {
+            using var scope = app.Services.CreateScope();
+            var db = scope.ServiceProvider.GetRequiredService<SecretSealDbContext>();
+            _ = await db.Database.EnsureCreatedAsync().ConfigureAwait(false);
+        }
+
+        await app.RunAsync().ConfigureAwait(false);
+    }
+}
