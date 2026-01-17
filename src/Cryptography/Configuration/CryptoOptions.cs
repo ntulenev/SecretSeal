77cc3cf63cc8df66d61d8e5493eadeaf100dcc81
Nikita Tulenev <n1tulenev@gmail.com>
2026-01-17 10:41:34 +0100

    Adds CryptoOptions configuration class
    Introduces a configuration class to manage cryptographic settings.
    
    This class provides a structured way to configure and access cryptographic options,
    such as the AES key used for encryption and decryption. The key is expected to be
    a Base64-encoded string.

---
+++ src/Cryptography/Configuration/CryptoOptions.cs
@@ -0,0 +1,12 @@
+﻿namespace Cryptography.Configuration;
+
+/// <summary>
+/// Represents configuration options for cryptographic operations.
+/// </summary>
+public sealed class CryptoOptions 
+{
+    /// <summary>
+    /// AES key used for encryption and decryption, represented as a Base64-encoded string.
+    /// </summary>
+    public required string Key { get; init; }
+}
