36bf341c829ac861834cf248bb79014ec311d535
Nikita Tulenev <n1tulenev@gmail.com>
2026-01-22 07:34:31 +0100

    Adds ShortGuidJsonConverter for serialization
    Ensures that the serialization process utilizes the `ShortGuidJsonConverter` when converting `Guid` values to JSON.
    This change aims to serialize the `id` property in a more compact format.

--- src/Transport.Tests/CreateNoteResponse.Tests.cs
+++ src/Transport.Tests/CreateNoteResponse.Tests.cs
@@ -2,6 +2,8 @@ using FluentAssertions;
 
 using System.Text.Json;
 
+using Transport.Serialization;
+
 namespace Transport.Tests;
 
 public sealed class CreateNoteResponseTests
@@ -10,18 +12,19 @@ public sealed class CreateNoteResponseTests
     [Trait("Category", "Unit")]
     public void SerializeUsesCamelCaseIdProperty()
     {
-        //Arrage
+        // Arrange
         var id = Guid.NewGuid();
         var response = new CreateNoteResponse(id);
         var options = new JsonSerializerOptions
         {
             PropertyNamingPolicy = JsonNamingPolicy.CamelCase
         };
+        options.Converters.Add(new ShortGuidJsonConverter());
 
         //Act
         var json = JsonSerializer.Serialize(response, options);
 
         //Assert
-        json.Should().Be($"{{\"id\":\"{id}\"}}");
+        json.Should().Be($"{{\"id\":\"{new ShortGuid(id)}\"}}");
     }
 }
