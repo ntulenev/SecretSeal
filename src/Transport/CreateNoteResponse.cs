39556d3f58bf3f54e2f81cb5fd9cce8d622a8040
Nikita Tulenev <n1tulenev@gmail.com>
2026-01-22 07:34:25 +0100

    Updates note ID to use ShortGuid
    Updates the `CreateNoteResponse` to use `ShortGuid` instead of `Guid` for the note ID.
    This change aims to improve storage efficiency and potentially enhance performance.

--- src/Transport/CreateNoteResponse.cs
+++ src/Transport/CreateNoteResponse.cs
@@ -4,4 +4,4 @@ namespace Transport;
 /// Represents the result of a create note operation, including the unique identifier of the newly created note.
 /// </summary>
 /// <param name="Id">The unique identifier assigned to the newly created note.</param>
-public sealed record CreateNoteResponse(Guid Id);
+public sealed record CreateNoteResponse(ShortGuid Id);
