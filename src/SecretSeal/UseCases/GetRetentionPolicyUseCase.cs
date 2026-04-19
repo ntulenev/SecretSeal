1bb83135b4d6630b51aed90f7fd4f14e9837783d
Nikita Tulenev <n1tulenev@gmail.com>
2026-04-19 13:54:30 +0200

    Update xmldoc in src/SecretSeal/UseCases/GetRetentionPolicyUseCase.cs

--- src/SecretSeal/UseCases/GetRetentionPolicyUseCase.cs
+++ src/SecretSeal/UseCases/GetRetentionPolicyUseCase.cs
@@ -8,8 +8,16 @@ using Transport;
 
 namespace SecretSeal.UseCases;
 
+/// <summary>
+/// Returns the effective note retention policy for the API.
+/// </summary>
 internal sealed class GetRetentionPolicyUseCase
 {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="GetRetentionPolicyUseCase"/> class.
+    /// </summary>
+    /// <param name="notesCleanerOptions">The configured note cleanup options.</param>
+    /// <param name="storageOptions">The configured storage options.</param>
     public GetRetentionPolicyUseCase(
         IOptions<NotesCleanerOptions> notesCleanerOptions,
         IOptions<StorageOptions> storageOptions)
@@ -20,6 +28,10 @@ internal sealed class GetRetentionPolicyUseCase
         _storageOptions = storageOptions;
     }
 
+    /// <summary>
+    /// Returns the effective retention policy visible to API consumers.
+    /// </summary>
+    /// <returns>The current retention policy response.</returns>
     public RetentionPolicyResponse Execute()
     {
         var daysToKeep = _storageOptions.Value.Mode == StorageMode.InMemory
